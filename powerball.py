b361f8b3b727cf87cfeb6883e2a915505fe4d48c
Jad Driggers <jaddriggers@gmail.com>
2023-12-31 14:17:16 -0500

    Added Beautiful Soup package and updated functionalities
    Integrated Beautiful Soup into the Python package list to scrape Powerball data from the web and display the information in the sidebar. Updated code to add scraping functionality and adjusted the placement of certain buttons from sidebar to main page. Updated packages and dependencies in Pipfile and Pipfile.lock.

--- powerball.py
+++ powerball.py
@@ -3,7 +3,27 @@ import random
 import pandas as pd
 from datetime import datetime
 from sqlalchemy import create_engine
+import requests
+from bs4 import BeautifulSoup
 
+# send a GET request
+response = requests.get('https://www.powerball.com/')
+
+# create a BeautifulSoup object and specify the parser
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# find the tag : <span class='game-jackpot-number text-xxxl lh-1 text-center'
+tag = soup.find('span', {'class': 'game-jackpot-number text-xxxl lh-1 text-center'})
+date_tag = soup.find('h5', {'class': 'card-title mx-auto mb-3 lh-1 text-center title-date'})
+# extract the content inside the tag
+draw_date = date_tag.text
+jackpot = tag.text
+
+# display the draw date
+st.sidebar.write(f"The next Powerball draw date is {draw_date}")
+
+# display the jackpot
+st.sidebar.write(f"The current Powerball jackpot is {jackpot}")
 
 def generate_powerball_numbers():
     numbers = random.sample(range(1, 70), 5)
@@ -34,7 +54,7 @@ if 'df' in st.session_state:
     st.write(st.session_state.df)
     if not st.session_state.df.empty:
         selected_id = st.sidebar.selectbox('Select an ID to delete:', st.session_state.df.index)
-        if st.sidebar.button('Delete selected ID'):
+        if st.button('Delete selected ID'):
             st.session_state.df = st.session_state.df.drop(selected_id)
             st.session_state.df = st.session_state.df.reset_index(drop=True)
             st.session_state.df.to_sql('powerball_numbers', con=engine, if_exists='replace', index=False)
@@ -42,8 +62,8 @@ if 'df' in st.session_state:
             df_updated['numbers'] = df_updated['numbers'].apply(lambda x: list(map(int, x.split(','))))
             st.session_state.df = df_updated
 
-# Sidebar widget for deleting all entries
-if st.sidebar.button("Delete all entries"):
+#  button for deleting all entries
+if st.button("Delete all entries"):
     st.session_state.df = st.session_state.df.iloc[0:0]
     st.session_state.df.to_sql('powerball_numbers', con=engine, if_exists='replace', index=False)
     df_updated = pd.read_sql('powerball_numbers', con=engine)
